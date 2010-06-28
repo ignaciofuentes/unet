a59861829be5077b24a38cc390938294e263547b
Juan Andres <jalagrange@gmail.com>
2010-06-28 13:51:50 -0430

    anadi clase container a style css

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,9 @@ ActiveRecord::Schema.define(:version => 20100626023637) do
     t.text     "descripcion"
     t.date     "fecha"
     t.string   "direccion"
+    t.integer  "grupo_estudiantil_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "estado_id"
-    t.integer  "grupo_estudiantil_id"
   end
 
   create_table "assignments", :force => true do |t|
@@ -67,6 +66,18 @@ ActiveRecord::Schema.define(:version => 20100626023637) do
     t.datetime "updated_at"
   end
 
+  create_table "permissions", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "permissions_roles", :id => false, :force => true do |t|
+    t.integer "permission_id"
+    t.integer "role_id"
+  end
+
   create_table "representantes", :force => true do |t|
     t.integer  "user_id",              :null => false
     t.integer  "grupo_estudiantil_id", :null => false
@@ -110,8 +121,6 @@ ActiveRecord::Schema.define(:version => 20100626023637) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role_id"
-    t.boolean  "verified",            :default => false
     t.boolean  "active",              :default => false, :null => false
   end
 
