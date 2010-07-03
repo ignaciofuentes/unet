307640e3dbf7aceca44b87b223a4fc989d96d821
unknown <Nacho@.(none)>
2010-07-02 20:38:36 -0430

    Elefante ha muerto

---
+++ public/javascripts/ckeditor/config.js
@@ -0,0 +1,41 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+  config.PreserveSessionOnFileBrowser = true;
+  // Define changes to default configuration here. For example:
+
+  // config.uiColor = '#AADC6E';
+
+  //config.ContextMenu = ['Generic','Anchor','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ;
+  
+  config.height = '400px';
+  config.width = '600px';
+  
+  config.resize_enabled = false;
+  config.language = 'es';
+  //config.resize_maxHeight = 2000;
+  //config.resize_maxWidth = 750;
+  
+  //config.startupFocus = true;
+  
+  // works only with en, ru, uk languages
+  config.extraPlugins = "embed,attachment";
+  
+  config.toolbar = 'Easy';
+  
+  config.toolbar_Easy =
+    [
+        ['Source','-','Preview'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord',],
+        ['Maximize'],
+        ['Bold','Italic','Underline','Strike','-', 'TextColor'],
+        ['NumberedList','BulletedList','-','Outdent','Indent'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink'],
+        ['Image','Embed','Flash','Attachment','HorizontalRule','Smiley','SpecialChar']
+    ];
+};
