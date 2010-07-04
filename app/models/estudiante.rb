937e3ba87a8f0297fdb6aebeff3adfab92400951
unknown <Nacho@.(none)>
2010-07-03 23:03:31 -0430

    done for tonight

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -5,13 +5,22 @@ class Estudiante < ActiveRecord::Base
 	 validates_presence_of :email
 	 validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 	 validates_presence_of :universidad_id, :message=> "no existe en el sistema"
+	 validates_length_of :inicial_segundo, :maximum => 1 
 	
 	def universidad_nombre
 		universidad.nombre if universidad
 	end
 	
 	def universidad_nombre=(nombre)
-	self.universidad=Universidad.find_by_nombre(nombre) unless nombre.blank?
+		self.universidad=Universidad.find_by_nombre(nombre) unless nombre.blank?
+	end
+	
+	def nombre_completo
+		[nombre, inicial_segundo_con_punto, apellido].compact.join(' ')
+	end
+	
+	def inicial_segundo_con_punto
+		"#{inicial_segundo}. " unless inicial_segundo.blank?
 	end
 	
 end
--- test/unit/estudiante_test.rb
+++ test/unit/estudiante_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class EstudianteTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
+def test_full_name_without_middle_initial  
+     estudiante = Estudiante.new(:nombre => "John", :apellido => "Smith")  
+     assert_equal 'John Smith', estudiante.nombre_completo
+   end  
+   
+   def test_full_name_with_middle_initial   
+     estudiante = Estudiante.new(:nombre => "Paul", :inicial_segundo => "P", :apellido => "Hughes")  
+     assert_equal 'Paul P. Hughes', estudiante.nombre_completo  
+   end  
+   
+   def test_full_name_with_empty_middle_initial  
+     estudiante = Estudiante.new(:nombre => "John", :inicial_segundo => "", :apellido => "Jones")  
+     assert_equal 'John Jones', estudiante.nombre_completo  
+   end  
+ end  
+
 end
